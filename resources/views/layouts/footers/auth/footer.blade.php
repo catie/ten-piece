3bd778f120bee4357c59cabc91338ccd9d034ea8
Catie Donnelly <catie@users.noreply.github.com>
2024-10-12 21:56:41 -0400

    Initial commit

---
+++ resources/views/layouts/footers/auth/footer.blade.php
@@ -0,0 +1,23 @@
+<footer class="footer pb-4">
+    <div class="container-fluid">
+        <div class="row align-items-center justify-content-lg-between">
+            <div class="col-lg-6 mb-lg-0 mb-4">
+                <div class="copyright text-center text-sm text-muted text-lg-left">
+                    © {{ now()->year }} Ten Piece by <a style="color: #252f40;" href="https://buildos.club" class="font-weight-bold ml-1"
+                        target="_blank">The Buildos' Club</a>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                    <li class="nav-item">
+                        <a href="https://buildos.club" class="nav-link text-muted" target="_blank">The Buildos' Club</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://fake.url/license" class="nav-link pe-0 text-muted"
+                            target="_blank">License</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/layouts/footers/guest/description.blade.php
@@ -0,0 +1,15 @@
+<footer class="footer pt-8 position-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 mb-4 mx-auto text-center">
+                <a href="https://buildos.club" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                    The Buildos' Club
+                </a>
+                <a href="https://fake.url" target="_blank"
+                    class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                    About Us
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
